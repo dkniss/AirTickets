f818fd1a56d503a1e1475164ac6baea6b16c203e
Daniil <danielkniss@mail.ru>
2021-06-27 11:31:32 +0500

    added LocationService and MapPrice

---
+++ AirTickets/AirTickets/Services/LocationService.h
@@ -0,0 +1,18 @@
+//
+//  LocationService.h
+//  AirTickets
+//
+//  Created by Daniil Kniss on 27.06.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+#define kLocationServiceDidUpdateCurrentLocation @"LocationServiceDidUpdateCurrentLocation"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LocationService : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
